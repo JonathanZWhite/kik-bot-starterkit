9a88ae66669e85abed638899a18f1dc70aca0237
Jonathan White <mrwhite@Jonathans-MacBook-Pro-2.local>
2016-04-06 16:53:37 -0400

    fixed merge conflict :bear

--- server.js
+++ server.js
@@ -14,40 +14,11 @@ const bot = new Bot({
   baseUrl: __CONFIG__.kik.baseUrl
 })
 
-<<<<<<< HEAD
 // updates configuration and auth
 bot.updateBotConfiguration()
 
 // incoming message handlers
 hooks(bot)
-=======
-// initial authentication
-request('POST', 'https://api.kik.com/v1/config')
-  .auth(__CONFIG__.kik.botUsername, __CONFIG__.kik.apiKey)
-  .send({
-    "webhook": __CONFIG__.kik.webhook + '/incoming',
-    "features": {
-       "manuallySendReadReceipts": false,
-       "receiveReadReceipts": false,
-       "receiveDeliveryReceipts": false,
-       "receiveIsTyping": false
-    }
-  })
-  .end((err, resp) => {
-    if (err) {
-      return logger.log('error', err);
-    }
-
-    logger.log('info', 'authentication success')
-  });
-
-// proof of successful connection
-bot.onTextMessage((message) => {
-  logger.log('info', 'message received')
-
-  message.reply(message.body);
-});
->>>>>>> 545260f8e47ff691115bff13705a78236f17eec6
 
 // Set up your server and start listening
 let server = http
